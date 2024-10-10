46edfa19d502c4d0ca2ae7f6ed642b0f9badb42c
williams <williamsgoddey11@gmail.com>
2024-10-10 16:58:43 +0100

    env file

--- src/components/BillInformation.jsx
+++ src/components/BillInformation.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSearchParams } from 'react-router-dom';
 import { optionSplit } from '../utils/dateFormat';
 import BillModal from './BillModal';
@@ -6,27 +6,39 @@ import Modals from './Modals';
 
 export default function BillInformation() {
   const [searchParams] = useSearchParams();
+  const [firstName,setFirstName]=useState("")
+  const [lastName,setLastName]=useState("")
+  const [department,setDepartment]=useState("")
+  const [regNumber,setRegNumber]=useState("")
+  const [academicLevel,setAcademicLevel]=useState("")
+  // const [option,setOption]=useState(searchParams.get("option"))
   const option = searchParams.get("option");
   console.log(option);
 
 
   const className = "max-w-[1250px]   w-[90vw]  m-auto";
   return (
-    <div className={`${className} min-h-screen flex justify-between  gap-x-6`}>
+    <form className={`${className} min-h-screen flex justify-between  gap-x-6`}>
       <div className="bg-white rounded-md p-4 self-start w-[300px] shadow-lg">
         <h4 className="border-b capitalize font-semibold p-4">bill type</h4>
         <div className="flex gap-x-2 border border-stone-400 p-2 rounded-md">
-          <input type="radio" checked className="self-start" />
+          <input
+            type="radio"
+            checked
+            className="self-start"
+            value={option}
+            
+          />
           <div>
             <h4 className="mb-0">{optionSplit(option)}</h4>
             <h4 className="mb-0">1000</h4>
           </div>
         </div>
-       <OpenModal/>
+        <OpenModal />
       </div>
 
-      <div className='flex flex-col gap-y-4 flex-1'>
-        <h4 className='font-semibold capitalize mb-0'>student information</h4>
+      <div className="flex flex-col gap-y-4 flex-1">
+        <h4 className="font-semibold capitalize mb-0">student information</h4>
 
         <div className="bg-white rounded-md p-3 flex justify-between gap-x-3">
           <div className="flex flex-col gap-y-3 flex-1">
@@ -87,7 +99,7 @@ export default function BillInformation() {
             />
           </div>
         </div>
-        
+
         <div className="bg-white rounded-md p-3 grid grid-cols-2 gap-x-3">
           <div className="flex flex-col gap-y-3 ">
             <div className="flex gap-x-2 items-center">
@@ -96,21 +108,21 @@ export default function BillInformation() {
                 5.current academic level
               </label>
             </div>
-           <select className='border border-stone-600 rounded-md p-3'>
-            <option>select level</option>
-            <option>select level</option>
-            <option>select level</option>
+            <select className="border border-stone-600 rounded-md p-3">
+              <option>select level</option>
+              <option>select level</option>
+              <option>select level</option>
             </select>
           </div>
 
-       <div className='flex justify-end items-end'>
-        <button className='bg-secondary400  rounded-md px-8 py-2 text-white capitalize font-heading font-semibold'>continue</button>
-       </div>
+          <div className="flex justify-end items-end">
+            <button className="bg-secondary400  rounded-md px-8 py-2 text-white capitalize font-heading font-semibold">
+              continue
+            </button>
+          </div>
         </div>
-
-
       </div>
-    </div>
+    </form>
   );
 }
 
